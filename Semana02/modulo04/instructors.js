c668bedab1fa789660dd8149d802e146a5e5f0e7
Hilton1 <josehilton02@gmail.com>
2021-10-04 20:16:47 -0300

    Inserindo dados no arquivo data.json corretamente.

--- Semana02/modulo04/instructors.js
+++ Semana02/modulo04/instructors.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 const data = require('./data.json');
 
 // create
@@ -13,7 +12,22 @@ exports.post = function(req, res) {
     }
   }
 
-  data.instructors.push(req.body);
+  let { avatar_url, birth, name, services, gender } = req.body;
+  
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.instructors.length + 1);
+
+
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+  });
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
     if (err) {
