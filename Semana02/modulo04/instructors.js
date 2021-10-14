4791af255e37d3a92c6679187d0665599b6c519e
Hilton1 <josehilton02@gmail.com>
2021-10-13 23:26:47 -0300

    Adding functions to view and edit data

--- Semana02/modulo04/instructors.js
+++ Semana02/modulo04/instructors.js
@@ -1,5 +1,27 @@
 const fs = require('fs');
 const data = require('./data.json');
+const { age, date } = require('./utils');
+
+// show
+exports.show = function(req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id == id;
+  });
+
+  if(!foundInstructor) return res.send('Instructor not found!');
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(', '),
+    created_at: Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+  }
+
+  return res.render('instructors/show', { instructor });
+
+}
 
 // create
 exports.post = function(req, res) {
@@ -37,4 +59,22 @@ exports.post = function(req, res) {
     return res.redirect("/instructors")
 })
 
+}
+
+// edit
+exports.edit = function(req, res) {
+  const { id } = req.params;
+  
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) return res.send('Instructor not found!');
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth),
+  }
+
+  return res.render('instructors/edit.njk', { instructor });
 }
