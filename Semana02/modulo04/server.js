440a19bf188d5b10b227d1d0cbc277b068765010
Hilton1 <josehilton02@gmail.com>
2021-10-17 00:02:59 -0300

    Atualizando dados com PUT

--- Semana02/modulo04/server.js
+++ Semana02/modulo04/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const methodOverride = require('method-override');
 
 const server = express();
 
 // Configura arquivos estáticos (css, js...)
 server.use(express.static('public'));
 server.use(express.urlencoded({ extended: true }))
+server.use(methodOverride('_method'));
 server.use(routes);
 
 // Configura as views
